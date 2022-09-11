f524fadee533cd49088b2c4631d6504de3329eb5
Faizal-Ardhavy <79374894+Faizal-Ardhavy@users.noreply.github.com>
2022-09-11 23:40:19 +0700

    push

--- dashboard/pembayaran.php
+++ dashboard/pembayaran.php
@@ -44,10 +44,6 @@
           </a>
           <ul class="mt-6">
             <li class="relative px-6 py-3">
-              <span
-                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                aria-hidden="true"
-              ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
                 href="dashboard.php"
@@ -94,6 +90,7 @@
               </a>
             </li>
             <li class="relative px-6 py-3">
+            <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                 href="pembayaran.php"
