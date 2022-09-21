cfd66e8f0978e1670a7a0a2f1625d83ae5d8062b
Faizal-Ardhavy <79374894+Faizal-Ardhavy@users.noreply.github.com>
2022-09-21 19:33:23 +0700

    push

--- signUp/signUp.php
+++ signUp/signUp.php
@@ -1,5 +1,5 @@
 <?php
-    require "../function.php";
+require "../function.php";
 
 ?>
 
@@ -21,97 +21,39 @@
 </head>
 
 <body>
-    <html>
+    <div class="row">
+        <div class="col-0 col-md-3"></div>
+        <div class="col-12 col-md-6 mx-auto">
+            <form action="signUp_action.php" method="post">
+                <h1> Pendaftaran Akun Peserta </h1>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Form Pendaftaran</title>
-        <link rel="stylesheet" href="https://codepen.io/gymratpacks/pen/VKzBEp#0">
-        <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/main.css">
-    </head>
+                <fieldset class="mb-3">
+                    <legend><span class="number">1</span> Profil</legend>
 
-    <body>
-        <div class="row">
-            <div class="col-md-12">
-                <form action="signUp_action.php" method="post">
-                    <h1> Pendaftaran Akun Peserta </h1>
+                    <label for="name">Email</label>
+                    <input type="email" id="username" name="username">
 
-                    <fieldset class="mb-3">
-                        <legend><span class="number">1</span> Profil</legend>
+                    <label for="name">Nama Lengkap</label>
+                    <input type="text" id="name" name="name">
 
-                        <label for="name">Email</label>
-                        <input type="email" id="username" name="username">
+                    <label for="telpon">No.Telpon</label>
+                    <input type="number" id="telp" name="telp">
 
-                        <label for="name">Nama Lengkap</label>
-                        <input type="text" id="name" name="name">
+                    <label for="school">Instansi</label>
+                    <input type="text" id="school" name="school">
 
-                        <label for="telpon">No.Telpon</label>
-                        <input type="number" id="telp" name="telp">
-                        
-                        <label for="school">Instansi</label>
-                        <input type="text" id="school" name="school">
-                        
-                        <label for="password">Password</label>
-                        <input type="password" id="pass" name="pass">
+                    <label for="password">Password</label>
+                    <input type="password" id="pass" name="pass">
 
 
-                    </fieldset>
-                    <!-- <fieldset>
-                        <legend><span class="number">2</span> Profil Anggota</legend>
+                </fieldset>
 
-                        <label for="name_member_1">Nama Anggota 1</label>
-                        <input type="text" id="name_member_1" name="name_member_1">
+                <button type="submit">Buat Akun</button>
 
-                        <label for="name_member_2">Nama Anggota 2</label>
-                        <input type="text" id="name_member_2" name="name_member_2">
-
-                        <legend><span class="number">4</span> Profil Tim</legend>
-
-                        <label for="school">Asal Sekolah</label>
-                        <input type="text" id="school" name="school"> -->
-
-                        <!-- <label for="job">Job Role:</label>
-                        <select id="job" name="user_job">
-                            <optgroup label="Web">
-                                <option value="frontend_developer">Front-End Developer</option>
-                                <option value="php_developer">PHP Developer</option>
-                                <option value="python_developer">Python Developer</option>
-                                <option value="rails_developer">Rails Developer</option>
-                                <option value="web_designer">Web Designer</option>
-                                <option value="wordpress_developer">Wordpress Developer</option>
-                            </optgroup>
-                            <optgroup label="Mobile">
-                                <option value="android_developer">Android Developer</option>
-                                <option value="ios_developer">IOS Developer</option>
-                                <option value="mobile_designer">Mobile Designer</option>
-                            </optgroup>
-                            <optgroup label="Business">
-                                <option value="business_owner">Business Owner</option>
-                                <option value="freelancer">Freelancer</option>
-                            </optgroup>
-                        </select> -->
-
-                        <!-- <label>Interests:</label>
-                        <input type="checkbox" id="development" value="interest_development" name="user_interest"><label
-                            class="light" for="development">Development</label><br>
-                        <input type="checkbox" id="design" value="interest_design" name="user_interest"><label
-                            class="light" for="design">Design</label><br>
-                        <input type="checkbox" id="business" value="interest_business" name="user_interest"><label
-                            class="light" for="business">Business</label> -->
-
-                    <!-- </fieldset> -->
-
-                    <button type="submit">Buat Akun</button>
-
-                </form>
-            </div>
+            </form>
         </div>
-
-    </body>
-
-    </html>
+        <div class="col-0 col-md-3"></div>
+    </div>
 
 </body>
 
